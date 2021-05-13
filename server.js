8b45006e073d61b322a5545c154b395beaf9b3c0
Robin Fussell <rfussell17@gmail.com>
2021-05-13 16:04:46 -0700

    server connected, mongo connected

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
-const method = require('method-override');
 
 const Todo = require('./models/todo');
 
-mongoose.connect('mongodb://localhost:27017/farmStand', {useNewUrlParser: true})
+mongoose.connect('mongodb://localhost:27017/todoApp', {useNewUrlParser: true})
 .then(()=>{
   console.log('mongo connection open')
 })
@@ -14,3 +13,12 @@ mongoose.connect('mongodb://localhost:27017/farmStand', {useNewUrlParser: true})
   console.log('mongo connection error')
   console.log(err)
 })
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
