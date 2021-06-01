fcbef66406ffb2f272c0733787f508be9a5bacd1
Robin F <rfussell17@gmail.com>
2021-06-01 16:49:40 -0700

    edited README and root route

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
-app.get("/todos", async (req, res) => {
+app.get("/", async (req, res) => {
   const todos = await Todo.find({});
   console.log(todos);
   res.render("todos/index", { todos });
