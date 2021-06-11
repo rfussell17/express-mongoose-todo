3a77d769468830bb65ce24629cb8c8275b0f1040
Robin F <rfussell17@gmail.com>
2021-06-11 15:49:11 -0700

    add express session

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+// const session = require('express-session');
+// const flash = require('connect-flash');
+// const ejsMate = require('ejs-mate');
+// const ExpressError = require('./helpers/ExpressError');
 
 const Todo = require("./models/todo");
 
